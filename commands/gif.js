10bc4ab2e76ec7376835e7b8292dc4b3d82e9d8c
Roly Fentanes <roly426@gmail.com>
2014-05-28 15:48:38 -0400

    %20

--- commands/gif.js
+++ commands/gif.js
@@ -17,7 +17,8 @@ module.exports = function(item, callback) {
       giphy.search(query, limit, offset, function(err, data) {
         if (err) return callback(err);
         if (!data || !data.data || !data.data.length) {
-          return callback(new Error('no gifs found for "' + query + '"'));
+          return callback(new Error(
+            'no gifs found for "' + decodeURIComponent(query) + '"'));
         }
 
         data = data.data;
